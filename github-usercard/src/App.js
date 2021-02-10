3c72ea73ab4bcfeacc2077d197d5e5e4fe95c36c
Chaz Carbis <chazcarbis@gmail.com>
2021-02-09 23:02:00 -0600

    done

--- github-usercard/src/App.js
+++ github-usercard/src/App.js
@@ -1,25 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import axios from 'axios'
+import UserCard from './UserCard'
+import Friends from './Friends'
 
-function App() {
-  return (
+class App extends React.Component {
+
+  state = {
+      bio: '',
+      email: '',
+      name: '',
+      avatar: '',
+      followers: 0,
+      login: [],
+
+  };
+
+ 
+  componentDidMount() {
+    axios
+       .get('https://api.github.com/users/dustinmyers')
+       .then((res) => {
+          this.setState({
+            bio: res.data.bio,
+            name: res.data.name,
+            avatar: res.data.avatar_url,
+            followers: res.data.followers,
+          })
+         })
+        .catch(err => console.log(`ERROR FETCHING DATA: ${err}`))
+       
+
+    axios
+      .get('https://api.github.com/users/dustinmyers/followers')
+      .then((res) => {
+        res.data.map((friend) => {
+          this.setState({
+            login: friend.name,
+            })
+          })
+        })
+        .catch(err => console.log(`ERROR FETCHING DATA: ${err}`))
+      
+      }
+
+  render() {
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UserCard bio= {this.state.bio} name={this.state.name} avatar={this.state.avatar} followers={this.state.followers } />
+      <Friends login= {this.state.login} />
+
+       
     </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ github-usercard/src/Friends.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class Friends extends React.Component {
+    render() {
+        const {
+            login,
+        } = this.props;
+
+        return (
+            <div className= 'Friend-Cards'>
+                {/* {login.map((user) => { */}
+                <h3>{login}</h3>
+                {/* })} */}
+                
+            </div>
+            )
+    }
+}
+
+export default Friends;
